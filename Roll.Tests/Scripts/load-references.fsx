d39d24e8cfbc6224ddb690765a9f56d2be0c37af
Max Wilson <wilson.max@gmail.com>
2015-09-13 16:52:10 -0700

    Add console app

--- Roll.Tests/Scripts/load-references.fsx
+++ Roll.Tests/Scripts/load-references.fsx
@@ -1,7 +1,6 @@
 // Warning: generated file; your changes could be lost when a new file is generated.
 #I __SOURCE_DIRECTORY__
 #r @"..\..\packages\FsCheck.2.0.7\lib\net45\FsCheck.dll"
-#r @"..\..\Roll\bin\Roll.dll"
 #r @"System.Core.dll"
 #r @"System.dll"
 #r @"System.Numerics.dll"
---
+++ RollExe/Program.fs
@@ -0,0 +1,26 @@
+﻿open mdw
+
+// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+
+
+[<EntryPoint>]
+let main argv = 
+    let (|StringMatch|) (args: string list) (input : string) =
+        if List.exists((=) (input.ToLowerInvariant())) args then
+            true
+        else
+            false
+    let rec loop() =
+        printf ">"
+        match System.Console.ReadLine().Trim() with
+        | StringMatch [""] true -> printfn "Eh?"; loop()
+        | StringMatch ["q"; "quit"] true -> ()
+        | v -> 
+            try
+                printfn "%d" (mdw.Parser.Parse v |> mdw.Dice.Instance.Resolve)
+            with e -> printfn "%s" (e.Message)
+            loop()
+    loop()
+    0
