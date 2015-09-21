68f7ce46283c0fe1c83143437f465314abdfeff5
Max Wilson <wilson.max@gmail.com>
2015-09-20 19:52:19 -0700

    Publish script

--- publish.cmd
+++ publish.cmd
@@ -2,6 +2,5 @@ git add . -A && git commit -m %1
 git checkout gh-pages
 git merge master
 msbuild Roll.sln
-git commit -a -m "Publish script"
-REM git push
+git commit -a -m "Publish script" && git push
 git checkout master
