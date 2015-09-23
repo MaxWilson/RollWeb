caea3e381b444e70d7e7652e2dae7cefb673acb7
Max Wilson <wilson.max@gmail.com>
2015-09-22 21:08:11 -0700

    Publish script update

--- publish.cmd
+++ publish.cmd
@@ -1,10 +1,6 @@
 git checkout gh-pages
 git merge master
 msbuild Roll.sln
-<<<<<<< HEAD
-git commit -a -m "Publish script" && git push
-=======
 git commit -a -m "Update JavaScript from master branch"
 git push
->>>>>>> master
 git checkout master
