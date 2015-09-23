43c8ba6bd78bcee35396e19f13d481a09fe5b2de
Max Wilson <wilson.max@gmail.com>
2015-09-22 21:35:11 -0700

    Update JavaScript from master branch

--- Roll/Content/RollWeb.js
+++ Roll/Content/RollWeb.js
@@ -17680,7 +17680,7 @@ var JSON;JSON||(JSON={}),function(){"use strict";function i(n){return n<10?"0"+n
       if(cmd.$==1)
        {
         result=this.Average(cmd.$0);
-        x=result.toFixed(2);
+        x=(result+1E-05).toFixed(2);
         return[Strings.EndsWith(x,".00")?String(result):x,"Computed"];
        }
       else
