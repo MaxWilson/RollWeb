35969a1c121bb5b3bff5d87ec3b242d44c3bb0c1
Max Wilson <wilson.max@gmail.com>
2015-10-05 09:00:08 -0700

    wip

--- Roll/Packrat.fs
+++ Roll/Packrat.fs
@@ -7,151 +7,126 @@ The following memo function allows construction of left-recursive grammars using
 iterated algorithm. Don't mess with the code in this file unless you've read the computer
 science papers involved and thoroughly understand them, and also have unit tests for the change
 you want to make.
+
+Okay, it's somewhat less deep magic now, since I rewrote it in continuation-passing style.
 *)
 
-
 type Input = string * int
-type Rule<'a> = string * (Input -> ('a * Input) option)
-type Head<'a> = { headRule: Rule<'a>; mutable involved: Set<string>; mutable eval: Set<string> }
-type LR<'a> = { mutable seed: ('a * Input) option; rule: Rule<'a>; mutable head: Head<'a> option; mutable rest: LR<'a> option }
-type Ans<'a> = LR of LR<'a> | Ans of 'a option
-type Memo<'a> = { mutable ans: Ans<'a>; mutable pos : Input }
-
-/// ParserContext holds all the "global" state for the parser. Memoized results, LR stack, etc.
-/// Not thread-safe for multiple threads--you'll get wrong answers if more than once logic stream
-/// is writing to these variables simultaneously.
-type ParserContext<'a>() =
-    let mutable heads: Map<Input, Head<'a>> = Map.empty
-    let mutable memorized = Map.empty
-    member this.InitializeHead(input: Input, head: Head<'a>) =
-        heads <- heads |> Map.add input head
-    member this.ClearHead(input: Input) =
-        heads <- heads |> Map.remove input
-    member this.Head(input: Input) =
-        Map.tryFind input heads
-    member this.Memo(name : string, input : Input) : Memo<'a> option =
-        Map.tryFind (name, input) memorized
-    member this.Memorize(name : string, input : Input, memo: Memo<'a>) =
-        memorized <- Map.add (name, input) memo memorized
-    member val LRStack : LR<'a> option = None with get, set
-    member this.Debug = heads, memorized
-    member this.PushLR(lr) =
-        this.LRStack <- Some lr
-    member this.PopLR() =
-        if this.LRStack.IsSome then
-            this.LRStack <- this.LRStack.Value.rest
+type Result<'a> = 
+    // Memoized parse result: either an AST and the rest of the input, or failure
+    | Memo of ('a * Input) option 
+    // A transformer which takes (name, rule, input) and produces a parse result 
+    | Func of ((string * (Input -> ('a * Input) option) * Input) -> ('a * Input) option)
+type ParserContext<'a>() = 
+    let mutable mem = Map.empty
+    let mutable callStack = []
+    member this.Memo(name : string, input : Input) : Result<'a> option =
+        Map.tryFind (name, input) mem
+    member this.Memorize(name, input, value) =
+        mem <- Map.add (name, input) value mem
     member this.Reset() =
-        heads <- Map.empty
-        memorized <- Map.empty
+        mem <- Map.empty
+    member this.Begin(name, input) =
+        callStack <- (name, input) :: callStack
+    member this.Return(name, input, result) =
+        match callStack with
+        | (n,i)::tail when n = name && i = input ->
+            callStack <- tail
+        | _ ->
+            Util.nomatch() // should never happen if memoize is working correctly
+        result // return result for convenience
+    member this.CallStack = callStack
+    member val RecordInvolved = None with get, set
+    member this.Memory = mem
 
-let memoize (ctx: ParserContext<'a>) =
-    let grow((name, rule): Rule<'a>, input, memo, head) = 
-        ctx.InitializeHead(input, head)
-        let mutable break1 = false
-        while not break1 do
-            head.eval <- head.involved
-            match rule input with
-            | None ->
-                break1 <- true
-            | Some(v, (s, i)) when i <= (snd memo.pos) ->
-                break1 <- true
-            | Some(v, (s, i)) ->
-                memo.ans <- Ans(Some v)
-                memo.pos <- (s, i)
-        ctx.ClearHead(input)
-        match memo.ans with
-        | LR(_) -> Util.nomatch()
-        | Ans(None) -> None
-        | Ans(Some(v)) -> Some(v, memo.pos)
-    let recall((name, rule): Rule<'a>, input) : Memo<'a> option =
-        let m = ctx.Memo(name, input)
-        let h = ctx.Head(input)
-        match m, h with
-        // if not growing, just return whatever is memoized
-        | _, None -> m
-        // do not evaluate any rule that is not involved
-        | None, Some(h) when not (name = (fst (h.headRule)) || (Set.contains name h.involved)) ->
-            None
-        // when growing left-recursively, evaluate involved rules, but only once per parser position
-        | _, Some(h) when Set.contains name h.eval ->
-            h.eval <- Set.remove name h.eval
-            let ans = ((h.headRule) |> snd) input
-            match m, ans with
-            | Some(m), Some(v, rest) -> 
-                m.ans <- Ans(Some v)
-                m.pos <- rest
-            | Some(m), None -> 
-                m.ans <- Ans None
-            | _ -> Util.nomatch() // should always have a memoized entry available when growing left-recursively
-            m
-        | _ -> m
-    // (re-)initialize lr based on LRStack variable; compute involved set and head rule
-    let setupLeftRecursion((name, rule):Rule<'a>, lr: LR<'a>) =
-        let LRHead = 
-            match (lr.head) with
-            | None ->
-                let h = { headRule = (name, rule); involved = Set.empty; eval = Set.empty }
-                lr.head <- Some(h)
-                h
-            | Some(h) -> h
-        // Use 's' to walk LRStack until a loop end is detected, collecting results in h.involved
-        let mutable s = ctx.LRStack
-        let getHead (lro : LR<'a> option) = 
-            lro 
-            |> Option.bind (fun lro -> 
-                                lro.head 
-                                |> function 
-                                    | Some(h) -> Some(h.headRule |> fst) 
-                                    | None -> None)
-        // Note: Does not have to be the same as name
-        let headName = getHead (Some lr)
-        while s.IsSome && (getHead s <> headName) do
-            s.Value.head <- lr.head
-            LRHead.involved <- Set.add (fst s.Value.rule) LRHead.involved
-            s <- s.Value.rest
+let rec firstTime (ctx: ParserContext<'a>) (name, rule, input) =
+    // grow a seed, collecting all left recursions as involved
+    let involved = ref Set.empty
+    let leftRecursion = ref false
+    let prevInvolved = ctx.RecordInvolved
+    ctx.RecordInvolved <- 
+        Some(fun callStack ->
+            if prevInvolved.IsSome then
+                prevInvolved.Value callStack 
+            let cycles = 
+                match callStack with
+                | (h,_)::t -> 
+                    if h = name then
+                        leftRecursion := true
+                    Seq.takeWhile (fst >> ((<>)h)) t
+                | _ -> Util.nomatch()
+            if !leftRecursion then
+                involved := Set.union (Set.ofSeq cycles) !involved)
+    let seed = rule input
+    // tidy up
+    ctx.RecordInvolved <- prevInvolved
+    if !leftRecursion then
+        grow ctx !involved (name, rule, input, seed)
+    else
+        ctx.Memorize(name, input, Memo(seed))
+        seed
+and grow (ctx: ParserContext<'a>) involved (name, rule, input, seed) =
+    for (rule, startpos) in involved do
+        ctx.Memorize(rule, startpos, Func (evalOnce (ref true)))
+    let rec loop prev =
+        ctx.Memorize(name, input, Memo(prev))
+        match rule input, prev with
+        | Some(v, ((_, i) as next)) as ans, Some(_, (_, prevNext)) when i > prevNext ->
+            // We grew! Keep growing
+            loop ans
+        | _ -> 
+            // I'm not sure about this cleanup phase
+            for (rule, startpos) in involved do
+                ctx.Memorize(rule, startpos, Func (lrAnswer ctx))
+            // Done. Clean up and return
+            prev
+    loop seed
+and evalOnce eval (name, rule, input)=
+    if !eval then
+        None
+    else
+        eval := false
+        let retval = rule input
+        eval := true
+        retval    
+and lrAnswer (ctx: ParserContext<'a>)  (name, rule, input)=
+    // Since we're now done with seed-growing, memoize the current answer
+    // I'm not sure if this is actually the right thing to do... may not account well
+    // for multiple heads. But I don't have a solid repro in my mind for where this
+    // would cause problems.
+    let ans = rule input
+    ctx.Memorize(name, input, Memo(ans))
+    ans      
+and fail(name : string, rule: (Input -> ('a * Input) option), input : Input) =
+    None
 
-    // Calls grow if this rule is the head of left recursion, otherwise just
-    // participate by returning the seed while the head grows instead.
-    // Seed = largest parse with no left recursion.
-    let growOrDefer((name,rule) : Rule<'a>, input : Input, memo : Memo<'a>) =
-        match memo.ans with
-        | LR(lr) ->
-            let h = lr.head
-            match h with
-            | Some(h) when fst h.headRule <> name ->
-                lr.seed
-            | Some(h) ->
-                memo.ans <- Ans (Option.map fst lr.seed)
-                match memo.ans with
-                | Ans None -> None
-                | _ -> grow((name,rule), input, memo, h)
-            | _ -> Util.nomatch()
-        | _ -> Util.nomatch()
-                 
-    fun name rule input -> 
-        match recall((name, rule), input) with
-        | None ->
-            let lr = { seed = None; rule = (name, rule); head = None; rest=ctx.LRStack }
-            ctx.PushLR(lr)
-            let m = { ans = (LR lr); pos = input }
-            ctx.Memorize(name, input, m)
-            let ans = rule input
-            ctx.PopLR()
-            match ans with
-            | Some(v, pos) ->
-                m.pos <- pos
-            | _ -> ()
-            match lr.head with
-            | Some(h) ->
-                lr.seed <- ans
-                growOrDefer((name, rule), input, m)
-            | None ->
-                m.ans <- Ans (Option.map fst ans)
-                ans
-        | Some(m) ->
-            match m.ans with
-            | LR(lr) ->
-                setupLeftRecursion((name, rule), lr)
-                lr.seed
-            | Ans(None) -> None
-            | Ans(Some(v)) -> Some(v, m.pos)
+let getProcessor (ctx: ParserContext<'a>) (name, rule, input) =
+    match ctx.Memo(name, input) with
+    // Already computed and memoized an answer
+    | Some(Memo(v)) ->
+        Memo(v)
+    // Already have a continuation specified
+    | Some(Func(f)) ->
+        Func(f)
+    | None ->
+        // First time at this position. Either it's the first time for this production
+        // period, or the production is already on the call stack higher up, which
+        // means we're growing a seed.
+        if List.exists (fst >> ((=) name)) ctx.CallStack then
+            ctx.RecordInvolved.Value(ctx.CallStack)
+            Func fail
+        else
+            Func (firstTime ctx)
+
+let memoize (ctx : ParserContext<'a>) =
+    // Depending on what we're doing right now, we could handle input in one of several ways
+            
+    let rec eval name rule input =
+        let p = getProcessor ctx (name, rule, input)
+        ctx.Begin(name, input)
+        match p with
+        | Memo(ans) -> 
+            ctx.Return(name, input, ans)
+        | Func(f) ->
+            ctx.Return(name, input, f(name, rule, input))
+    eval
--- Roll/Scripts/Script1.fsx
+++ Roll/Scripts/Script1.fsx
@@ -10,6 +10,7 @@ open mdw.DataDefs
 #r @"..\..\packages\xunit.runner.visualstudio.2.0.1\build\_common\xunit.abstractions.dll"
 #r @"..\..\packages\xunit.assert.2.0.0\lib\portable-net45+win+wpa81+wp80+monotouch+monoandroid+Xamarin.iOS\xunit.assert.dll"
 #r @"..\..\packages\xunit.extensibility.core.2.0.0\lib\portable-net45+win+wpa81+wp80+monotouch+monoandroid+Xamarin.iOS\xunit.core.dll"
+#r "System.Runtime"
 open Xunit
 #nowarn "0040"
 #nowarn "0058"
@@ -41,80 +42,90 @@ type ParserContext<'a>() =
     member this.CallStack = callStack
     member val RecordInvolved = None with get, set
 
-let memoize (ctx : ParserContext<'a>) =
-    let rec firstTime(name, rule, input) =
-        // grow a seed, collecting all left recursions as involved
-        let involved = ref Set.empty
-        ctx.RecordInvolved <- 
-            Some(fun callStack ->
-                let getCycles = function
-                | (h,_)::t -> Seq.takeWhile (fst >> ((<>)h)) t
+let rec firstTime (ctx: ParserContext<'a>) (name, rule, input) =
+    // grow a seed, collecting all left recursions as involved
+    let involved = ref Set.empty
+    let leftRecursion = ref false
+    let prevInvolved = ctx.RecordInvolved
+    ctx.RecordInvolved <- 
+        Some(fun callStack ->
+            if prevInvolved.IsSome then
+                prevInvolved.Value callStack 
+            let cycles = 
+                match callStack with
+                | (h,_)::t -> 
+                    if h = name then
+                        leftRecursion := true
+                    Seq.takeWhile (fst >> ((<>)h)) t
                 | _ -> Util.nomatch()
-                involved := Set.union (Set.ofSeq (getCycles callStack)) !involved)
-        let seed = rule input
-        // tidy up to make debugging easier
-        ctx.RecordInvolved <- None
-        if Set.isEmpty !involved then
-            ctx.Memorize(name, input, Memo(seed))
-            seed
-        else
-            grow !involved (name, rule, input)
-    and grow involved (name, rule, input) =
-        for (rule, startpos) in involved do
-            ctx.Memorize(rule, startpos, Func (evalOnce (ref true)))
-        let rec loop prev =
-            match rule input, prev with
-            | Some(v, ((_, i) as next)) as ans, Some(_, (_, prevNext)) when i > prevNext ->
-                // We grew! Keep growing
-                ctx.Memorize(name, input, Memo(ans))
-                loop ans
-            | _ -> 
-                // I'm not sure about this cleanup phase
-                for (rule, startpos) in involved do
-                    ctx.Memorize(rule, startpos, Func (lrAnswer))
-                // Done. Clean up and return
-                prev
-        loop None
-    and evalOnce eval (name, rule, input)=
-        if !eval then
-            None
-        else
-            eval := false
-            let retval = rule input
-            eval := true
-            retval    
-    and lrAnswer(name, rule, input)=
-        // Since we're now done with seed-growing, memoize the current answer
-        // I'm not sure if this is actually the right thing to do... may not account well
-        // for multiple heads. But I don't have a solid repro in my mind for where this
-        // would cause problems.
-        let ans = rule input
-        ctx.Memorize(name, input, Memo(ans))
-        ans      
-    let fail(name, rule, input) =
+            if !leftRecursion then
+                involved := Set.union (Set.ofSeq cycles) !involved)
+    let seed = rule input
+    // tidy up
+    ctx.RecordInvolved <- prevInvolved
+    if !leftRecursion then
+        grow ctx !involved (name, rule, input, seed)
+    else
+        ctx.Memorize(name, input, Memo(seed))
+        seed
+and grow (ctx: ParserContext<'a>) involved (name, rule, input, seed) =
+    for (rule, startpos) in involved do
+        ctx.Memorize(rule, startpos, Func (evalOnce (ref true)))
+    let rec loop prev =
+        ctx.Memorize(name, input, Memo(prev))
+        match rule input, prev with
+        | Some(v, ((_, i) as next)) as ans, Some(_, (_, prevNext)) when i > prevNext ->
+            // We grew! Keep growing
+            loop ans
+        | _ -> 
+            // I'm not sure about this cleanup phase
+            for (rule, startpos) in involved do
+                ctx.Memorize(rule, startpos, Func (lrAnswer ctx))
+            // Done. Clean up and return
+            prev
+    loop seed
+and evalOnce eval (name, rule, input)=
+    if !eval then
         None
+    else
+        eval := false
+        let retval = rule input
+        eval := true
+        retval    
+and lrAnswer (ctx: ParserContext<'a>)  (name, rule, input)=
+    // Since we're now done with seed-growing, memoize the current answer
+    // I'm not sure if this is actually the right thing to do... may not account well
+    // for multiple heads. But I don't have a solid repro in my mind for where this
+    // would cause problems.
+    let ans = rule input
+    ctx.Memorize(name, input, Memo(ans))
+    ans      
+and fail(name : string, rule: (Input -> ('a * Input) option), input : Input) =
+    None
 
+let getProcessor (ctx: ParserContext<'a>) (name, rule, input) =
+    match ctx.Memo(name, input) with
+    // Already computed and memoized an answer
+    | Some(Memo(v)) ->
+        Memo(v)
+    // Already have a continuation specified
+    | Some(Func(f)) ->
+        Func(f)
+    | None ->
+        // First time at this position. Either it's the first time for this production
+        // period, or the production is already on the call stack higher up, which
+        // means we're growing a seed.
+        if List.exists (fst >> ((=) name)) ctx.CallStack then
+            ctx.RecordInvolved.Value(ctx.CallStack)
+            Func fail
+        else
+            Func (firstTime ctx)
+
+let memoize (ctx : ParserContext<'a>) =
     // Depending on what we're doing right now, we could handle input in one of several ways
-    let getProcessor(name, rule, input) =
-        match ctx.Memo(name, input) with
-        // Already computed and memoized an answer
-        | Some(Memo(v)) ->
-            Memo(v)
-        // Already have a continuation specified
-        | Some(Func(f)) ->
-            Func(f)
-        | None ->
-            // First time at this position. Either it's the first time for this production
-            // period, or the production is already on the call stack higher up, which
-            // means we're growing a seed.
-            if List.exists (fst >> ((=) name)) ctx.CallStack then
-                ctx.RecordInvolved.Value(ctx.CallStack)
-                Func fail
-            else
-                Func firstTime
             
     let rec eval name rule input =
-        let p = getProcessor(name, rule, input)
+        let p = getProcessor ctx (name, rule, input)
         ctx.Begin(name, input)
         match p with
         | Memo(ans) -> 
@@ -124,22 +135,29 @@ let memoize (ctx : ParserContext<'a>) =
     eval
         
 type Expr = Leaf of char | Interior of Expr * Expr
-#nowarn "0040" // Allow object recursion without warnings so we can write recursive memoized rules
-[<Fact>]
+
 let (|Next|Empty|) = function
 | (input : string), pos when pos < input.Length -> Next(input.[pos], (input, pos+1))
 | _ -> Empty
-let show = sprintf "%A" 
+
 let c = ParserContext()
+// define an intermediate production "E" to make recursion indirect
 let rec (|Xs|_|) = memoize c "Xs" (function
-    | Xs(lhs, Next('+', Number(rhs, next))) -> Some(Interior(lhs, rhs), next)
-    | Number(v, next) -> Some(v, next)
+    | E(lhs, Next('+', Number(rhs, next))) -> Some(Interior(lhs, rhs), next)
+    | Next(c, next) when System.Char.IsDigit(c) -> Some(Leaf c, next)
+    | _ -> None)
+and (|E|_|) = memoize c "E" (function
+    | Xs(v, next) -> Some(v, next)
     | _ -> None)
 and (|Number|_|) = memoize c "Number" (function
     | Next(c, next) when System.Char.IsDigit(c) -> Some(Leaf c, next)
     | _ -> None)
+// It's an Xs, and it's also an E
 match("1+2+3",0) with
 | Xs(v, Empty) -> 
-    // Result should be left-associative
-    Assert.Equal(show <| Interior(Interior(Leaf('1'),Leaf('2')),Leaf('3')), show v)
+    Assert.Equal(Interior(Interior(Leaf('1'),Leaf('2')),Leaf('3')), v)
+| _ -> failwith "Could not parse"
+match("1+2+3",0) with
+| E(v, Empty) -> 
+    Assert.Equal(Interior(Interior(Leaf('1'),Leaf('2')),Leaf('3')), v)
 | _ -> failwith "Could not parse"
